83dfadef5146ac7450cfaafac54aa16863cc15e0
pr4gz3y <balintkaverebes@gmail.com>
2024-01-19 13:57:57 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Dataset0119
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.adatbazis = new System.Data.DataSet();
+            this.Szemelyek = new System.Data.DataTable();
+            this.dataColumn1 = new System.Data.DataColumn();
+            this.dataColumn2 = new System.Data.DataColumn();
+            this.dataColumn3 = new System.Data.DataColumn();
+            this.dataColumn4 = new System.Data.DataColumn();
+            this.dataColumn5 = new System.Data.DataColumn();
+            this.generatebutton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Fizatlag = new System.Windows.Forms.Label();
+            this.Eletatlag = new System.Windows.Forms.Label();
+            this.nonevek = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.adatbazis)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Szemelyek)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // adatbazis
+            // 
+            this.adatbazis.DataSetName = "NewDataSet";
+            this.adatbazis.Tables.AddRange(new System.Data.DataTable[] {
+            this.Szemelyek});
+            // 
+            // Szemelyek
+            // 
+            this.Szemelyek.Columns.AddRange(new System.Data.DataColumn[] {
+            this.dataColumn1,
+            this.dataColumn2,
+            this.dataColumn3,
+            this.dataColumn4,
+            this.dataColumn5});
+            this.Szemelyek.Constraints.AddRange(new System.Data.Constraint[] {
+            new System.Data.UniqueConstraint("Constraint1", new string[] {
+                        "Szemely_id"}, true)});
+            this.Szemelyek.PrimaryKey = new System.Data.DataColumn[] {
+        this.dataColumn1};
+            this.Szemelyek.TableName = "Szemelyek";
+            // 
+            // dataColumn1
+            // 
+            this.dataColumn1.AllowDBNull = false;
+            this.dataColumn1.AutoIncrement = true;
+            this.dataColumn1.ColumnName = "Szemely_id";
+            this.dataColumn1.DataType = typeof(int);
+            // 
+            // dataColumn2
+            // 
+            this.dataColumn2.ColumnName = "Nev";
+            this.dataColumn2.DefaultValue = "Nem ismert";
+            // 
+            // dataColumn3
+            // 
+            this.dataColumn3.ColumnName = "Nem";
+            this.dataColumn3.DefaultValue = "Ferfi";
+            // 
+            // dataColumn4
+            // 
+            this.dataColumn4.ColumnName = "Kor";
+            this.dataColumn4.DataType = typeof(int);
+            this.dataColumn4.DefaultValue = "18";
+            // 
+            // dataColumn5
+            // 
+            this.dataColumn5.ColumnName = "Fizetes";
+            this.dataColumn5.DataType = typeof(decimal);
+            this.dataColumn5.DefaultValue = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            // 
+            // generatebutton
+            // 
+            this.generatebutton.Location = new System.Drawing.Point(12, 12);
+            this.generatebutton.Name = "generatebutton";
+            this.generatebutton.Size = new System.Drawing.Size(75, 23);
+            this.generatebutton.TabIndex = 0;
+            this.generatebutton.Text = "Generate";
+            this.generatebutton.UseVisualStyleBackColor = true;
+            this.generatebutton.Click += new System.EventHandler(this.generatebutton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(680, 422);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Fizatlag
+            // 
+            this.Fizatlag.AutoSize = true;
+            this.Fizatlag.Location = new System.Drawing.Point(93, 17);
+            this.Fizatlag.Name = "Fizatlag";
+            this.Fizatlag.Size = new System.Drawing.Size(64, 13);
+            this.Fizatlag.TabIndex = 2;
+            this.Fizatlag.Text = "Átlag fizetés";
+            // 
+            // Eletatlag
+            // 
+            this.Eletatlag.AutoSize = true;
+            this.Eletatlag.Location = new System.Drawing.Point(211, 17);
+            this.Eletatlag.Name = "Eletatlag";
+            this.Eletatlag.Size = new System.Drawing.Size(66, 13);
+            this.Eletatlag.TabIndex = 3;
+            this.Eletatlag.Text = "Átlag életkor";
+            // 
+            // nonevek
+            // 
+            this.nonevek.FormattingEnabled = true;
+            this.nonevek.Location = new System.Drawing.Point(698, 41);
+            this.nonevek.Name = "nonevek";
+            this.nonevek.Size = new System.Drawing.Size(132, 225);
+            this.nonevek.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(961, 475);
+            this.Controls.Add(this.nonevek);
+            this.Controls.Add(this.Eletatlag);
+            this.Controls.Add(this.Fizatlag);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.generatebutton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.adatbazis)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Szemelyek)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Data.DataSet adatbazis;
+        private System.Data.DataTable Szemelyek;
+        private System.Data.DataColumn dataColumn1;
+        private System.Data.DataColumn dataColumn2;
+        private System.Data.DataColumn dataColumn3;
+        private System.Data.DataColumn dataColumn4;
+        private System.Data.DataColumn dataColumn5;
+        private System.Windows.Forms.Button generatebutton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label Fizatlag;
+        private System.Windows.Forms.Label Eletatlag;
+        private System.Windows.Forms.ListBox nonevek;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dataset0119
+{
+    public partial class Form1 : Form
+    {
+        Random vel = new Random();
+        string[] vezeteknev = { "Oláh", "Kovács", "Dombi", "Raffael", "Kolompár" };
+        string[] noikeresztnev = { "Anna", "Klári", "Sára", "Anita", "Zsófi" };
+        string[] ferfikeresztnev = { "BMW Individual M760Li xDrive Model V12", "Marci Csibész", "Samu King", "Bálint", "Milán" };
+        public Form1()
+        {
+            InitializeComponent();
+
+            //írj függvényt 'Data' névvel
+            //paraméterei: 'DataRow[], amiben a DataRowban szereplő mezők'
+            //visszatérési értéke: string, ami a html
+
+            DataSet adatbazis = new DataSet();
+            DataTable dt = new DataTable("Szemelyek");
+            DataColumn column = dt.Columns.Add("Szemely_id", typeof(int));
+            column.AutoIncrement = true;
+            column.AutoIncrementSeed = 1;
+            column.AutoIncrementStep = 1;
+
+            dt.PrimaryKey = new DataColumn[] { column };
+
+            dt.Columns.Add("Nev", typeof(string));
+            dt.Columns.Add("Nem", typeof(int));
+            dt.Columns.Add("Kor", typeof(int));
+            dt.Columns.Add("Fizetes", typeof(decimal));
+        }
+
+        private void generatebutton_Click(object sender, EventArgs e)
+        {
+            DataTable dt = adatbazis.Tables["Szemelyek"];
+
+            for (int i = 0; i < 5; i++)
+            {
+                DataRow dr = dt.NewRow();
+
+                if (vel.Next(1, 3) == 2)
+                {
+                    dr["Nem"] = "Ferfi";
+                    dr["Nev"] = $"{vezeteknev[vel.Next(vezeteknev.Length)]} {ferfikeresztnev[vel.Next(ferfikeresztnev.Length)]}";
+                }
+                else
+                {
+                    dr["Nem"] = "Nő";
+                    dr["Nev"] = $"{vezeteknev[vel.Next(vezeteknev.Length)]} {noikeresztnev[vel.Next(noikeresztnev.Length)]}";
+                }
+
+                dr["Kor"] = vel.Next(18, 60);
+                dr["Fizetes"] = vel.Next(350, 1200) * 1000;
+                dt.Rows.Add(dr);
+            }
+
+            dataGridView1.DataSource = dt;
+            dataGridView1.Columns["Nev"].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+            decimal salary = 0;
+            int db = 0;
+            foreach (DataRow dr in dt.Rows) 
+            {
+                salary += (decimal)dr["Fizetes"];
+                db++;
+            }
+
+            decimal avg = salary / db;
+            Fizatlag.Text = $"Átlag fizetés: {avg}";
+
+
+
+            int eletkor = 0;
+            int dbkor = 0;
+            foreach(DataRow dr in dt.Rows)
+            {
+                eletkor += (int)dr["Kor"];
+                dbkor++;
+            }
+
+            decimal avgelet = eletkor / dbkor;
+            Eletatlag.Text = $"Átlag életkor: {avgelet}";
+
+
+            DataRow[] rows = dt.Select("Nem = 'Nő'");
+            foreach (DataRow dr in rows)
+            {
+                nonevek.Items.Add(dr["Nev"]);
+            }
+
+            DataRow[] listb = dt.Select();
+            string[] mezok = { "Nev", "Nem", "Kor" };
+
+            DataToHTML(listb, mezok);
+
+        }
+
+        string DataToHTML(DataRow[] rows, string[] fields)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<html>");
+
+            sb.Append("<style>");
+            sb.Append("<table, tr {border: 1px, solid, black}>");
+            sb.Append("</style>");
+
+            sb.Append("<body>");
+
+            sb.Append("<talbe>");
+            sb.Append("<tr>");
+            foreach (string mezo in fields)
+            {
+                sb.Append($"<th> {mezo}");
+            }
+
+            return null;
+        }
+    }
+}
